9f80bf6dde2c4359f41c4bdebb295fb697c849ab
Дмитрий Панов <Lemrik33work@gmail.com>
2023-10-04 14:34:55 +0300

    23

--- Lessons_sc/dz_sc_22/Program.cs
+++ Lessons_sc/dz_sc_22/Program.cs
@@ -4,20 +4,75 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первый катет");
-            int firstCatet = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите второй катет");
-            int secondCatet = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < firstCatet; i++)
+            while (true)
             {
-                Console.Write("#");
-                for (int j = 1; j < secondCatet; j++)
-                {
-                    Console.Write("#");
-                }
+
+                Console.WriteLine("Введите катет");
+                int catet = int.Parse(Console.ReadLine());
+
                 Console.WriteLine();
+
+                // первый треугольник
+                for (int i = 0; i <= catet; i++)
+                {
+
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+
+                // второй треугольник
+                for (int i = catet; i >= 0; i--)
+                {
+
+                    for (int j = i; j > 0; j--)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+
+
+                // третий треугольник
+                for (int i = 0; i < catet; i++)
+                {
+                    for (int j = catet; j > (i + 1); j--)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+
+                // четвертый треугольник
+                for (int i = catet; i > 0; i--)
+                {
+                    for (int j = catet; j > i; j--)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+
+
+                Console.WriteLine("\nНажмите любую клавишу для отчистки консоли");
+                Console.ReadKey();
+                Console.Clear();
             }
         }
     }
---
+++ Lessons_sc/sc_v23/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace sc_v23
+{
+    // ТЕРНАРНЫЙ ОПЕРАТОР 
+    // выполняет действия над тремя операндами
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+
+            // унарный оператор
+            a++;
+
+            // бинарный оператор
+            a = a + 1;
+
+            // тернарный оператор
+            // [первый операнд - условие] ? [второй операнд if_true] : [третий операнд if_else]
+            while (true)
+            {
+
+                bool accessAllowed;
+
+                Console.WriteLine("Введите пароль: ");
+
+                string storedPassword = "qwerty";
+                string enteredPassword = Console.ReadLine();
+
+                //if (storedPassword == enteredPassword)
+                //{
+                //    accessAllowed = true;
+                //}
+                //else  
+                //{
+                //    accessAllowed = false;
+                //}
+
+                // тоже самое
+
+                accessAllowed = storedPassword == enteredPassword ? true : false;
+
+                Console.WriteLine(accessAllowed);
+                Console.ReadKey();
+            }
+        }
+    }
+}
