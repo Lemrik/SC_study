7de284e00d6629a9434b61e7ac2f0ec4e284336f
Дмитрий Панов <Lemrik33work@gmail.com>
2023-11-10 16:15:12 +0300

    35

---
+++ Lessons_sc/dz_sc_35/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace dz_sc_35
+{
+    internal class Program
+    {
+        // 1
+        // Написать метод, который выводит на экран строку.
+        // Символы из которых состоит строка и их количество вводятся пользователем
+
+        // 2
+        // Написать метод для поиска индекса элемента массива (тип элементов в массиве -int),
+        // метод должен вернуть индекс первого найденного элемента (если он будет найден)
+        static void Main()
+        {
+            // 1
+            string a = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+
+            PrintString(a, b);
+
+        }
+
+        // 1
+        static void PrintString(string symb, int col)
+        {
+            string result = "";
+            for (int i = 0; i < col; i++)
+            {
+                result = result + symb;
+            }
+            Console.WriteLine(result);
+        }
+
+        //static int SearchIndex(int c) 
+        //{
+        //    int[] arr;
+            
+        //    foreach (int i in c)
+        //    {
+
+        //    }
+        //}
+    }
+}
---
+++ Lessons_sc/sc_v35/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace sc_v35
+{
+    // C# ФУНКЦИИ И МЕТОДЫ
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = Sum(5, 7);
+            Console.WriteLine(c);
+
+            PrintLine();
+
+            PrintResult(4, 6);
+        }
+
+        static int Sum(int a, int b)
+        {
+            int result = a + b;
+
+            return result;
+        }
+
+        static void PrintLine()
+        {
+            Console.WriteLine("Метод PrintLine");
+        }
+
+        static void PrintResult(int value_1, int value_2)
+        {
+            int result = value_1 + value_2;
+            Console.WriteLine("Результат метода: " + result);
+        }
+    }
+}
